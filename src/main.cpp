877f995f080f0fef30c6d97625e4a0996d02e695
shlabisa <159113956+shlabisa@users.noreply.github.com>
2025-10-02 16:05:15 +0200

    Update main.cpp
    Added encoder reading and updated power calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 const int IN1 = 7;
 const int IN2 = 6;
 
@@ -8,30 +7,42 @@ const float refValue = 0.5;
 
 void setup() {
   Serial.begin(115200);
-  
+
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
-  
+
   pinMode(ENA, OUTPUT);
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
 }
 
 void loop() {
-   float potValue = analogRead(A0) / 1024.0;
-   
-   float power = potValue - refValue;
-   Serial.print("Power: ");
-   Serial.print(abs(power) * 200.0);
-   Serial.println("%");
-   analogWrite(ENA, int(abs(power * 255.0) * 2.0));
-   if(power == 0.0) {
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,LOW);
-   }else if(power > 0) {
-    digitalWrite(IN1,HIGH);
-    digitalWrite(IN2,LOW);
-   }else {
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,HIGH);
-   }
-   
+  float potValue = analogRead(A0) / 1024.0;
+  Serial.print("Potentiometer: ");
+  Serial.print(potValue * 100.0);
+  Serial.println(" %");
+
+  float encValue = analogRead(A1) / 1024.0;
+  Serial.print("Encoder: ");
+  Serial.print(encValue * 100.0);
+  Serial.println(" %");
+
+  //float power = potValue - refValue;
+  float power = potValue - encValue;
+  Serial.print("Power: ");
+  Serial.print(power * 100.0);
+  Serial.println(" %");
+  analogWrite(ENA, int(abs(power * 255.0) * 1.0));
+  if (power == 0.0) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  } else if (power > 0) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  } else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  }
+  Serial.println("\n");
 }

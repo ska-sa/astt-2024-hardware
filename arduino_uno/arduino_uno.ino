85bee898ed108bb0dfac96cb0516d60b35a8f025
shlabisa <shlabisa@sarao.ac.za>
2026-06-05 15:04:56 +0200

    Integrate calibrated rotate in azimuth code with find astt

--- arduino_uno/arduino_uno.ino
+++ arduino_uno/arduino_uno.ino
@@ -1,52 +1,16 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_GPS.h>
-
-// Create GPS object using I2C
-Adafruit_GPS GPS(&Wire);
-
-void GPSSetup() {
-  GPS.begin(0x10); // Default I2C address for PA1010D
-
-  // Optional: configure GPS output
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // GPS Recommended minimum + fix data
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-  GPS.sendCommand(PMTK_API_SET_FIX_CTL_1HZ); // Requests and checks the fix status (whether the GPS is locked)
-  GPS.sendCommand(PGCMD_ANTENNA); // Request antenna status
-  delay(1000);
-  GPS.println(PMTK_Q_RELEASE); // Ask for firmware version
-  return;
-}
-
-void GPSLoop() {
-  char c = GPS.read();
-  if (c) Serial.print(c);
-
-  if (GPS.newNMEAreceived()) {
-    if (!GPS.parse(GPS.lastNMEA())) return;
-    // Print parsed GPS data
-    Serial.print("Time: "); Serial.print(GPS.hour); Serial.print(":");
-    Serial.print(GPS.minute); Serial.print(":"); Serial.println(GPS.seconds);
-    Serial.print("Date: "); Serial.print(GPS.day); Serial.print("/");
-    Serial.print(GPS.month); Serial.print("/"); Serial.println(GPS.year);
-    Serial.print("Lat: "); Serial.println(GPS.latitude, 4);
-    Serial.print("Long: "); Serial.println(GPS.longitude, 4);
-    Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-    Serial.print("Altitude: "); Serial.println(GPS.altitude);
-    if (GPS.fix) {
-      Serial.println("GPS has a fix!");
-    } else {
-      Serial.println("No GPS fix yet.");
-    }
-  }
-}
+#include "find_astt.h"
+#include "rotate_in_azimuth.h"
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Initializing...");
     
     // Find the ASTT Setup
-    GPSSetup();
+    findASTTSetup();
+
+    // Rotate in Azimuth Setup
+    rotateInAzimuthSetup();
 
     Serial.println("Initialization complete.");
 }
@@ -54,7 +18,10 @@ void setup() {
 void loop() {
     
     // Find the ASTT Loop
-    GPSLoop();
+    findASTTLoop();
+
+    // Rotate in Azimuth Loop
+    rotateInAzimuthLoop();
 
     delay(10);
 }
